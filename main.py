e905ab153f30cfa11b11418de216e3e7f1d0d94d
elmaniobrero <43865794+elmaniobrero@users.noreply.github.com>
2025-06-27 17:11:18 -0600

    cambios en codigo

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 #**aqui va todo lo del codigo main osea este es el proyecto**#
 def main():
-    print(get_book_text())
+    print({number_words()}, "words found in the document")
 
 
 def get_book_text():
     with open('books/frankenstein.txt') as f:
         book_contents = f.read()
         return book_contents
+
+def number_words():
+    texto = get_book_text()
+    palabras = texto.split()
+    contador = 0
+    for p in palabras:
+        contador = contador + 1
+    return contador
+
+
 
 main()
