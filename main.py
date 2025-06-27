d68d163b5284052d4ab51f0767fa43cb6c8bdb16
elmaniobrero <43865794+elmaniobrero@users.noreply.github.com>
2025-06-27 16:19:05 -0600

    el main del programa funcionando

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+#**aqui va todo lo del codigo main osea este es el proyecto**#
+def main():
+    print(get_book_text())
+
+
+def get_book_text():
+    with open('books/frankenstein.txt') as f:
+        book_contents = f.read()
+        return book_contents
+
+main()
